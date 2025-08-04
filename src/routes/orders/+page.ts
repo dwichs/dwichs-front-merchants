ad082c20f4dc282e74b9ced9a7198dcd8581a80e
Karitchi <karichikarichikarichi@gmail.com>
2025-08-04 19:04:35 +0200

    remove unused `/orders` unused route

--- src/routes/orders/+page.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export async function load({ fetch }) {
-  const response = await fetch(`/api/orders`);
-  const userOrders = await response.json();
-
-  return { userOrders };
-}
