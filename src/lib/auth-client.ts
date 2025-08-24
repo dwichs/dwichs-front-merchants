5d99d6a3acca1f260e1875e2e033aa6fa49485c0
Karitchi <karichikarichikarichi@gmail.com>
2025-08-24 12:16:59 +0200

    change static auth url to env var

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/svelte";
+import { PUBLIC_API_BASE_CLIENT } from "$env/static/public";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000/api/auth",
+  baseURL: `${PUBLIC_API_BASE_CLIENT}/api/auth`,
 });
