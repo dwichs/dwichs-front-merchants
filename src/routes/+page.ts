dba0564cab0248de2304fcdbc063286b8efc538b
Karitchi <karichikarichikarichi@gmail.com>
2025-08-28 14:42:53 +0200

    remove unused files and fix api call by including credentials

--- src/routes/+page.ts
+++
@@ -1,6 +0,0 @@
-import { env } from "$env/dynamic/public";
-
-export const load = async ({ fetch }) => {
-  const res = await fetch(`${env.PUBLIC_API_BASE_CLIENT}/data`);
-  return { data: await res.json() };
-};
--- src/routes/restaurants/[[restaurant_name]]/[id]/orders/+page.ts
+++ src/routes/restaurants/[[restaurant_name]]/[id]/orders/+page.ts
@@ -4,7 +4,11 @@ import { env } from "$env/dynamic/public";
 export const load: PageLoad = async ({ fetch, params }) => {
   const response = await fetch(
     `${env.PUBLIC_API_BASE_CLIENT}/restaurants/${params.id}/orders`,
+    {
+      credentials: "include",
+    },
   );
+
   const orders = await response.json();
 
   return { orders };
