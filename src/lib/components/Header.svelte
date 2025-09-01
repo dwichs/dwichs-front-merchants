cbc8ffd2cd12d4f1518a143cbd3cb3d338dd0b49
Karitchi <karichikarichikarichi@gmail.com>
2025-09-01 19:59:24 +0200

    add hamburger menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,54 @@
 <script>
   import Title from "./header/Title.svelte";
-  import UserMenuTrigger from "./header/UserMenuTrigger.svelte";
+  import SignOut from "./header/SignOut.svelte";
+
+  let { userGroups } = $props();
+  let isOpen = $state(false);
+  let dropdownRef = $state();
+
+  function closeDropdown() {
+    isOpen = false;
+  }
+
+  function handleClickOutside(event) {
+    if (dropdownRef && !dropdownRef.contains(event.target)) {
+      closeDropdown();
+    }
+  }
+
+  $effect(() => {
+    if (isOpen) {
+      document.addEventListener("click", handleClickOutside);
+      return () => {
+        document.removeEventListener("click", handleClickOutside);
+      };
+    }
+  });
 </script>
 
-<div class="flex justify-between pt-10 items-center">
-  <div></div>
+<div class="flex justify-center p-5">
   <Title />
-  <UserMenuTrigger />
+  <div class="absolute flex gap-5 top-0 right-0 p-5 text-gray-800">
+    <div
+      bind:this={dropdownRef}
+      class=" relative rounded border border-gray-300 shadow flex items-center justify-between gap-2 z-10"
+    >
+      <button
+        onclick={() => (isOpen = !isOpen)}
+        class="cursor-pointer p-3 flex gap-2 items-center justify-between"
+      >
+        <span> Menu </span>
+        <span class="material-symbols-outlined"> menu </span>
+      </button>
+      {#if isOpen}
+        <div
+          class="absolute right-0 top-full mt-1 bg-white border border-gray-300 shadow-lg rounded min-w-32"
+        >
+          <div onclick={closeDropdown}>
+            <SignOut />
+          </div>
+        </div>
+      {/if}
+    </div>
+  </div>
 </div>
---
+++ src/lib/components/header/SignOut.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { authClient } from "$lib/auth-client";
+
+  let loading = false;
+
+  async function handleSignOut() {
+    loading = true;
+
+    const { data, error: err } = await authClient.signOut();
+
+    console.log(data);
+    console.log(err);
+
+    loading = false;
+
+    if (err) {
+      console.log("error");
+    } else {
+      goto("/sign-in");
+    }
+  }
+</script>
+
+<div class="p-2 flex items-center gap-2 group">
+  <span
+    class=" group-hover:bg-red-500 group-hover:text-white transition rounded-full p-1 material-symbols-outlined"
+  >
+    exit_to_app
+  </span>
+  <button onclick={handleSignOut} class="group-hover:underline cursor-pointer"
+    >Sign Out</button
+  >
+</div>
