1fffc65d4f791dde2cb969b1d046bf9ea82cc973
Karitchi <karichikarichikarichi@gmail.com>
2025-09-02 01:17:37 +0200

    redirect

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,28 @@
 export const ssr = false;
+
+/** @type {import('./$types').LayoutLoad} */
+import { PUBLIC_API_BASE_CLIENT } from "$env/static/public";
+import { browser } from "$app/environment";
+import { redirect } from "@sveltejs/kit";
+
+export async function load({ fetch, url }) {
+  // Fetch groups from the API endpoint
+  const res = await fetch(`${PUBLIC_API_BASE_CLIENT}/groups`, {
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    // If groups call fails and we're not on sign-in, redirect
+    if (browser && url.pathname !== "/sign-in") {
+      throw redirect(302, "/sign-in");
+    }
+    console.error(`Failed to fetch groups: ${res.status}`);
+    const userGroups = [];
+    return { userGroups };
+  }
+
+  const userGroups = await res.json();
+  return {
+    userGroups,
+  };
+}
